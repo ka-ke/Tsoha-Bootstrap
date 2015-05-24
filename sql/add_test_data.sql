e950967d214bb260a0aaf2a7eeb8679db53ad702
Kasperi Jaakkola <kasperi.jaakkola@helsinki.fi>
2015-05-24 18:23:16 +0300

    Testitaulukoita

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Collection (collector) VALUES ('Sami');
+INSERT INTO CollectionEntry (added, cardname, expansion, foil, cardshape, amount, location) 
+VALUES (NOW(), 'Dark Confidant', 'Modern Masters', true, 'Near-mint', 1, 'Kansiossa');
