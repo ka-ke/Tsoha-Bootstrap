c711513f0ff633648cbfb6a12c9f03e5ef30650b
Kasperi Jaakkola <kasperi.jaakkola@helsinki.fi>
2015-05-24 18:12:17 +0300

    Taulukoita

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,18 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Collection(
+    ID SERIAL PRIMARY KEY,
+    collector varchar(20) NOT NULL,
+);
+
+CREATE TABLE CollectionEntry(
+    ID SERIAL PRIMARY KEY,
+    added DATE,
+    collectorID INTEGER REFERENCES Collection(ID),
+    cardname varchar(20) NOT NULL,
+    expansion varchar(20) NOT NULL,
+    foil boolean DEFAULT FALSE,
+    condition varchar(20) NOT NULL,
+    amount INTEGER NOT NULL,
+    location varchar(20)
+);
+
--- sql/drop_tables.sql
+++ sql/drop_tables.sql
@@ -1,1 +1,3 @@
 -- Lisää DROP TABLE lauseet tähän tiedostoon
+DROP TABLE IF EXISTS Collection CASCADE;
+DROP TABLE IF EXISTS CollectionEntry CASCADE;
